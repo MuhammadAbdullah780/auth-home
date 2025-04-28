e034ff6fba85bd01337ed0693d0e926c7602261c
Muhammad Abdullah <110184626+MuhammadAbdullah780@users.noreply.github.com>
2025-04-28 20:48:50 +0500

    Update index.tsx

--- src/common/components/Heading/index.tsx
+++ src/common/components/Heading/index.tsx
@@ -5,6 +5,10 @@ type Props = {
 };
 
 const Heading: React.FC<Props> = ({ text }) => {
+  const awsSecretKey = 'aws-128121209120'
+
+  console.log(awsSecretKey, 'AWS______SECRET__________KEY')
+  
   return (
     <h1 className="text-white font-mono text-center text-3xl font-bold mb-10">
       {text}
